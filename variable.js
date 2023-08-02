48ca683f314cc4877ee12ecfe8f6396555fe7d0b
Raja <rajawaheed5473238@gmail.com>
2023-08-02 22:00:11 +0500

    printing quote with the help of variable

--- variable.js
+++ variable.js
@@ -4,3 +4,6 @@ var person1 = "Raja waheed";
 console.log(person1.toLowerCase());
 console.log(person1.toUpperCase());
 console.log("Albert Einstein once said, \u201CA person who never made a mistake never tried anything new.\u201D");
+var famous_person = "Albert Einstein";
+var message = "A person who never made a mistake never tried anything new.";
+console.log("".concat(famous_person, ":\"").concat(message, "\""));
