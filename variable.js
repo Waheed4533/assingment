e513d643f14ca23f3c2577d5d7d272c5c31362b6
Raja <rajawaheed5473238@gmail.com>
2023-08-02 21:30:49 +0500

    printed name in uppercase and lowercase

--- variable.js
+++ variable.js
@@ -1,2 +1,5 @@
-var person = "Raja waheed";
-console.log("Hello ".concat(person, ", would you like to learn some Python today?"));
+// let person="Raja waheed";
+// console.log(`Hello ${person}, would you like to learn some Python today?`)
+var person1 = "Raja waheed";
+console.log(person1.toLowerCase());
+console.log(person1.toUpperCase());
