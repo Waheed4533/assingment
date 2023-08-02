eef7dc6ce60ff5469ae7a2ce790078bff80af82b
Raja <rajawaheed5473238@gmail.com>
2023-08-02 22:15:36 +0500

    printing name with and without spaces

--- variable.js
+++ variable.js
@@ -1,9 +1,12 @@
-var person = "Raja waheed";
-console.log("Hello ".concat(person, ", would you like to learn some Python today?"));
-var person1 = "Raja waheed";
-console.log(person1.toLowerCase());
-console.log(person1.toUpperCase());
-console.log("Albert Einstein once said, \u201CA person who never made a mistake never tried anything new.\u201D");
-var famous_person = "Albert Einstein";
-var message = "A person who never made a mistake never tried anything new.";
-console.log("".concat(famous_person, ":\"").concat(message, "\""));
+// let person="Raja waheed";
+// console.log(`Hello ${person}, would you like to learn some Python today?`)
+// let person1="Raja waheed";
+// console.log(person1.toLowerCase())
+// console.log(person1.toUpperCase())
+// console.log(`Albert Einstein once said, “A person who never made a mistake never tried anything new.”`)
+// let famous_person="Albert Einstein"
+// let message="A person who never made a mistake never tried anything new."
+// console.log(`${famous_person}:"${message}"`)
+var name1 = "    Raja Waheed     ";
+console.log(name1);
+console.log(name1.trim());
