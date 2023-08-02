0e380fff7c00bc987545168b8587213094d31a95
Raja <rajawaheed5473238@gmail.com>
2023-08-02 22:32:09 +0500

    printing message of my fav num through variable

--- variable.js
+++ variable.js
@@ -18,7 +18,9 @@
 // console.log(subtraction);
 // let division=16/2;
 // console.log(division)
-console.log(5 + 3);
-console.log(16 - 8);
-console.log(4 * 2);
-console.log(16 / 2);
+// console.log(5+3);
+// console.log(16-8);
+// console.log(4*2);
+// console.log(16/2);
+var favorite_number = 5;
+console.log("My favorite number is ".concat(favorite_number));
